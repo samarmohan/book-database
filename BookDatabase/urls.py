afbacd6f988954bf93f78b93a780f307ff572145
Samar <samar.mohan@gmail.com>
2021-01-01 17:27:12 -0600

    Make urls consistent

--- BookDatabase/urls.py
+++ BookDatabase/urls.py
@@ -22,13 +22,13 @@ from books.api import CreateAPI, ListAPI, RetrieveAPI, UpdateAPI, DeleteAPI
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('add-book/', add_book_view),
-    path('<title>/update', update_view),
-    path('<title>/detail', detail_view),
-    path('api/books/create', CreateAPI.as_view()),
-    path('api/books', ListAPI.as_view()),
-    path('api/books/<Title>/detail', RetrieveAPI.as_view()),
-    path('api/books/<Title>/update', UpdateAPI.as_view()),
-    path('api/books/<Title>/delete', DeleteAPI.as_view()),
+    path('<title>/update/', update_view),
+    path('<title>/detail/', detail_view),
+    path('api/books/create/', CreateAPI.as_view()),
+    path('api/books/', ListAPI.as_view()),
+    path('api/books/<Title>/detail/', RetrieveAPI.as_view()),
+    path('api/books/<Title>/update/', UpdateAPI.as_view()),
+    path('api/books/<Title>/delete/', DeleteAPI.as_view()),
     path('', home_view)
 ]
 
