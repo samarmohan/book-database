47177271743188773ddc222f276b0ea7d8a4658a
Samar <samar.mohan@gmail.com>
2021-02-06 09:35:49 -0600

    Add more search queries. (Still no styling)

--- books/views.py
+++ books/views.py
@@ -14,9 +14,22 @@ def add_book_view(request):
 def home_view(request):
     book_response_home_list = BookModel.objects.all()
     title_contains_query = request.GET.get("title")
+    author_contains_query = request.GET.get("author")
+    name_contains_query = request.GET.get("name")
+    rating_contains_query = request.GET.get("rating")
 
     if title_contains_query != "" and title_contains_query is not None:
         book_response_home_list = book_response_home_list.filter(Title__icontains=title_contains_query)
+
+    elif author_contains_query != "" and author_contains_query is not None:
+        book_response_home_list = book_response_home_list.filter(Author__icontains=author_contains_query)
+
+    elif name_contains_query != "" and name_contains_query is not None:
+        book_response_home_list = book_response_home_list.filter(Name__icontains=name_contains_query)
+
+    elif rating_contains_query != "" and rating_contains_query is not None:
+        book_response_home_list = book_response_home_list.filter(Rating__icontains=rating_contains_query)
+
     return render(request, "home.html", {"books": book_response_home_list})
 
 
