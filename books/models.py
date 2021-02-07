b9efaec57eace4c97dfdb18b97cc099d7bb8005c
Samar <samar.mohan@gmail.com>
2021-02-07 16:18:05 -0600

    Change the text of ContainsBadWords.

--- books/models.py
+++ books/models.py
@@ -9,6 +9,6 @@ class BookModel(models.Model):
     Author = models.CharField(max_length=255, blank=False, null=False, verbose_name="Author of the book")
     Description = models.TextField(blank=True, null=True, verbose_name="Summary of the book (Optional)")
     PageCount = models.PositiveIntegerField(blank=True, null=True, verbose_name="Number of pages in the book (Optional)")
-    ContainsBadWords = models.BooleanField(verbose_name="Does this book contain bad words? (Bad for an 8 year old)", null=True, blank=True)
+    ContainsBadWords = models.BooleanField(verbose_name="Is this book inappropriate for someone around 8 years old?", null=True, blank=True)
     Rating = models.PositiveIntegerField(validators=[MinValueValidator(0), MaxValueValidator(10)], null=False, blank=False, verbose_name="Rating out of 10")
 
